548c0e805c5689338ecfb4754206bcc950f79d30
hardik <hp676913@gmail.com>
2022-07-05 17:17:21 +0530

    Apply Insort Api

--- app.js
+++ app.js
@@ -3,39 +3,38 @@ let newsBox = document.getElementById('newsBox');
 // Create XML Object
 const xhr = new XMLHttpRequest();
 
-let key = "5ee41684e9124d3382b0e5416bb89437";
-let source = "bbc-news";
-xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${key}`, true);
 
-xhr.getResponseHeader('Content-type', 'application/json');
+xhr.open('GET', "https://inshorts.deta.dev/news?category=national", true);
+
+// xhr.getResponseHeader('Content-type', 'application/json');
 
 xhr.onload = function () {
     // status code 200 is For "OK"
     if (this.status === 200) {
         let json = JSON.parse(this.responseText);
-
-        let articles = json.articles;  // Get Articles Oblect
-        console.log(articles);
+        let data = json.data;  // Get data Oblect
+        console.log(data);
         let newsHTML = "";
 
-        //Applying Loop on articles to get inner elements
-        for (key in articles) {
+        //Applying Loop on data to get inner elements
+        for (key in data) {
 
-            function getCurrentDate() {
-                let dateString = `${articles[key].publishedAt}`;
-                dateString = dateString.slice(0, 10);
-                return `${dateString.slice(8, 10)} / ${dateString.slice(5, 7)} / ${dateString.slice(0, 4)}`;
-            }
+            // function getCurrentDate() {
+            //     let dateString = `${data[key].publishedAt}`;
+            //     dateString = dateString.slice(0, 10);
+            //     return `${dateString.slice(8, 10)} / ${dateString.slice(5, 7)} / ${dateString.slice(0, 4)}`;
+            // }
             let news = `<div class="newsCard card">
-                            <img src="${articles[key].urlToImage}"
+                            <img src="${data[key].imageUrl}"
                                 class="img ard-img-top img-thumbnail" alt="Image">
-                            <h5 class="card-header">${articles[key].title}</h5>
+                            <h5 class="card-header">${data[key].title}</h5>
                             <div class="card-body">
-                                <h5 class="card-title">${articles[key].description}</h5>
-                                <p class="card-text">${articles[key].content}</p>
-                                <a target="_blank" href="${articles[key].url}" class="btn btn-primary">Read more..</a>
+                                <h5 class="card-title">Author: ${data[key].author}</h5>
+                                <p class="card-text">${data[key].content}</p>
+                                <a target="_blank" href="${data[key].readMoreUrl}" class="btn btn-primary">Read more..</a>
                             </div>
-                           <div class="text-center card-footer text-muted">${getCurrentDate()}</div>
+                           <div class="text-center card-footer text-muted">${data[key].date}</div>
+                           
                         </div>`;
             newsHTML += news;
         }
