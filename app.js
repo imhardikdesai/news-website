c3339c5b3c57d828b903bfa463e3969536d9a6dd
Hardik Desai <hp676913@gmail.com>
2022-07-21 21:31:38 +0530

    now we can find news by category

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 let newsBox = document.getElementById('newsBox');
 let spinner = document.getElementById('spinner');
-
+let newsCateogry = ['national', 'business', 'sports', 'world', 'politics', 'technology', 'startup', 'entertainment', 'miscellaneous', 'hatke', 'science', 'automobile'];
 // Create XML Object
 const xhr = new XMLHttpRequest();
 
+function sendCategory(index) {
+    getNews(newsCateogry[index])
+}
+getNews("all");
 
-xhr.open('GET', "https://inshorts.deta.dev/news?category=national", true);
+function getNews(newsCateogryName) {
+    xhr.open('GET', `https://inshorts.deta.dev/news?category=${newsCateogryName}`, true);
 
-xhr.getResponseHeader('Content-type', 'application/json');
+    xhr.getResponseHeader('Content-type', 'application/json');
 
+    xhr.onload = function () {
+        // status code 200 is For "OK"
+        if (this.status === 200) {
+            let json = JSON.parse(this.responseText);
+            let data = json.data;  // Get data Oblect
+            // console.log(data);
+            let newsHTML = "";
 
-
-xhr.onload = function () {
-    // status code 200 is For "OK"
-    if (this.status === 200) {
-        let json = JSON.parse(this.responseText);
-        let data = json.data;  // Get data Oblect
-        // console.log(data);
-        let newsHTML = "";
-
-        function showSpinner() {
-            setTimeout(() => {
+            function showSpinner() {
                 spinner.style.visibility = 'hidden';
                 newsBox.style.visibility = 'visible';
-                
-            }, 700);
-        }
+            }
 
-        xhr.onprogress = showSpinner();
+            xhr.onprogress = showSpinner();
 
-        //Applying Loop on data to get inner elements
-        for (key in data) {
+            //Applying Loop on data to get inner elements
+            for (key in data) {
 
-            let news = `<div class="newsCard card">
+                let news = `<div class="newsCard card">
                             <img src="${data[key].imageUrl}"
                                 class="img ard-img-top img-thumbnail" alt="Image">
                             <h5 class="card-header">${data[key].title}</h5>
@@ -44,17 +44,16 @@ xhr.onload = function () {
                            <div class="text-center card-footer text-muted">${data[key].date}</div>
                            
                         </div>`;
-            newsHTML += news;
+                newsHTML += news;
+            }
+            // Manuplating DOM
+            newsBox.innerHTML = newsHTML;
         }
-        // Manuplating DOM
-        newsBox.innerHTML = newsHTML;
+        else {
+            console.log("Some Error Occured");
+        }
     }
-    else {
-        console.log("Some Error Occured");
-    }
+
+    xhr.send();  // This is important to run whole code
+
 }
-
-xhr.send();  // This is important to run whole code
-
-
-
